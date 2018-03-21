5afb3f35ff8a0441fda781c0905b79e88d7a9e9a
Michael Calabrese <michael.calabrese@myob.com>
2018-03-21 14:11:20 +1100

    after jsp changes

--- src/main/java/MusicServlet.java
+++ src/main/java/MusicServlet.java
@@ -1,3 +1,4 @@
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -5,12 +6,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name="MusicServlet", urlPatterns = "/index")
+@WebServlet(name="MusicServlet", urlPatterns = "/")
 public class MusicServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setStatus(404);
+        resp.setStatus(200);
+        resp.setContentType("text/html");
+
+        req.getRequestDispatcher("index.jsp").include(req, resp);
+//        RequestDispatcher requestDispatcher = req.getRequestDispatcher("index.html");
+//        requestDispatcher.include(req,resp);
 
 
     }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: michael.calabrese
+  Date: 21/3/18
+  Time: 1:32 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+      <h1> JSP -  WEB PAGES, AM I RIGHT?</h1>
+  </body>
+</html>
